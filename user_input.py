b205c86d04dbc1f83b34347dd8c99d37f6ea81ba
Julio Lito <juliusmidega3@gmail.com>
2024-02-25 20:15:15 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+location = input("where do you come from?: ")
+
+print(f"Hello {name}, you are {age} year old and live in {location}.")
